d3d494d44fedff4534cfd85729f605a31f7f35a6
Dhruv patel <100340700+idhruvchaudhary@users.noreply.github.com>
2022-03-08 00:20:09 +0530

    Add files via upload

---
+++ petern41.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class petern41 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int i, j, k;
+        for (i = 3; i >= 0; i--) {
+            for (k = 0; k <= 3 - i; k++) {
+                System.out.print(" ");
+            }
+            for (j = 0; j <= i; j++) {
+                System.out.print(" *");
+            }
+            System.out.print("\n");
+        }
+
+        for (i = 0; i <= 3; i++) {
+            for (k = 0; k <= 3 - i; k++) {
+                System.out.print(" ");
+            }
+            for (j = 0; j <= i; j++) {
+                System.out.print(" *");
+            }
+            System.out.print("\n");
+        }
+    }
+
+}
